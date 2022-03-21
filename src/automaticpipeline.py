26e4ac1f091475045f471a016111c540420ceb2e
amsgn-iscteiul <amsgn@iscte-iul.pt>
2022-03-21 22:21:00 +0000

    Remove shell command

--- src/automaticpipeline.py
+++ src/automaticpipeline.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 
@@ -9,7 +8,6 @@ def main():
         for count, directory in enumerate(os.listdir(sys.argv[1])):
             patient_identification = '/Patient10' + str(count) + '_'
             converter.full_convert(sys.argv[1] + '/' + directory, sys.argv[2], sys.argv[3], patient_identification)
-    os.system('nnUnet_predict -i ' + sys.argv[3] + ' -o' + sys.argv[4] + '-t 27 -m 2d --save')
 
 
 if __name__ == '__main__':
