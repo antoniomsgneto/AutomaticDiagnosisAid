ffbf601eee0de15ffe423584f418a7ca28828e9d
amsgn-iscteiul <amsgn@iscte-iul.pt>
2022-05-03 22:48:14 +0100

    fixes background image retrieval

--- src/apply_snakes.py
+++ src/apply_snakes.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
         for file in listdir(sys.argv[1]):
             if file.endswith('nii.gz'):
                 print("here")
-                segment_patient(sys.argv[2] + file, sys.argv[1] + file, sys.argv[3])
+                segment_patient(sys.argv[2] + file.replace('.nii.gz','_0000.nii.gz'), sys.argv[1] + file, sys.argv[3])
