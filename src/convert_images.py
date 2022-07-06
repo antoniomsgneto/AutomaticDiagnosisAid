743cf6b100d983ac65f31532ce142dff1cf85d44
amsgn-iscteiul <amsgn@iscte-iul.pt>
2022-07-06 20:50:56 +0100

    Changed patient identification

--- src/convert_images.py
+++ src/convert_images.py
@@ -6,7 +6,7 @@ def main():
     from utilities import dicom2nifticonverter as converter
     if sys.argv[1]:
         for count, directory in enumerate(os.listdir(sys.argv[1])):
-            patient_identification = '/Patient10' + str(count) + '_'
+            patient_identification = directory
             converter.full_convert(sys.argv[1] + '/' + directory, sys.argv[2], sys.argv[3], patient_identification)
 
 
