6df9f29705b36e96a3706e4246c54dea5217360e
amsgn-iscteiul <amsgn@iscte-iul.pt>
2022-10-27 00:02:35 +0100

    Fix snake definition

--- segmentation/simple_segmentation.py
+++ segmentation/simple_segmentation.py
@@ -16,16 +16,22 @@ def open_nifti_image(path_to_file):
 def snake_segmentation_without_dots(image,frame):
     img = image.get_data()[:, :, frame]
     img_shape = img.shape
-
+    print(img_shape)
     #ax.imshow(img)
+
+
     np_pixdata = np.array(img)
+
+    an_array = np.where(np_pixdata != 3, 0, np_pixdata)
+    center_of_mass = ndimage.measurements.center_of_mass(an_array)
+
     # np_counter = np.count_nonzero(np_pixdata[(np_pixdata == 2)])
     an_array = np.where(np_pixdata != 2, 0, np_pixdata)
     img = rgb2gray(an_array)
 
     s = np.linspace(0, 2 * np.pi, 500)
-    r = 250 + 50 * np.sin(s)
-    c = 280 + 50 * np.cos(s)
+    r = center_of_mass[0] + 200 * np.sin(s)
+    c = center_of_mass[1] + 200 * np.cos(s)
     init = np.array([r, c]).T
     snake = active_contour(gaussian(img, 3),
                            init, boundary_condition="periodic",
@@ -39,7 +45,10 @@ def snake_segmentation_without_dots(image,frame):
                             init, boundary_condition="periodic",
                             alpha=0.015, beta=10, gamma=0.001, w_line=-0.2, w_edge=4, max_iterations=5000)
     snake_array = np.zeros([img_shape[0]*100, img_shape[1]*100])
+    print(snake)
     points_r, points_c = list(map(lambda x: round(x*100), snake[:, 0])), list(map(lambda x: round(x*100), snake[:, 1]))
+    print(points_r)
+    print(points_c)
     interior_r, interior_c = polygon(points_r, points_c)
     perimeter_r, perimeter_c = polygon_perimeter(points_r, points_c)
     snake_array[perimeter_r, perimeter_c] = 1
