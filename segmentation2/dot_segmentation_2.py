34bd204fd6fbd7b005b404887889e00970c779ca
amsgn-iscteiul <amsgn@iscte-iul.pt>
2023-04-26 18:51:24 +0100

    Adds percentage distance comparison

--- segmentation2/dot_segmentation_2.py
+++ segmentation2/dot_segmentation_2.py
@@ -74,7 +74,8 @@ fig_list = []
 
 
 def snake_segmentation_dots(
-    path_to_file
+    path_to_file,
+    path_to_origin
 ):
     multiple_frame_snakes = []
     multiple_frame_dots = []
@@ -82,6 +83,10 @@ def snake_segmentation_dots(
     multiple_frame_center_distances = []
 
     image = nib.load(path_to_file)
+    try:
+        image_origin = nib.load(path_to_origin)
+    except:
+        image_origin = None
     pixdim_xyz = image.header["pixdim"][1:4]
     pixel_area = pixdim_xyz[0] * pixdim_xyz[1]
 
@@ -110,12 +115,14 @@ def snake_segmentation_dots(
             print(e)
     print("Building Graphs")
     build_graphs(
-        image, multiple_frame_distances, multiple_frame_snakes, multiple_frame_dots, multiple_frame_center_distances
+        image_origin, image, multiple_frame_distances, multiple_frame_snakes, multiple_frame_dots,
+        multiple_frame_center_distances
     )
 
 
 def build_graphs(
-    image, multiple_frame_distances, multiple_frame_snakes, multiple_frame_dots, multiple_frame_center_distances
+    image_origin, image, multiple_frame_distances, multiple_frame_snakes, multiple_frame_dots,
+    multiple_frame_center_distances
 ):
     fig = plt.figure(figsize=(18, 9))
     gs = fig.add_gridspec(2, 4, width_ratios=[2, 1, 1, 1], height_ratios=[2, 2])
@@ -125,6 +132,7 @@ def build_graphs(
     ax3 = fig.add_subplot(gs[0, 3])
     ax4 = fig.add_subplot(gs[1, 2])
     ax5 = fig.add_subplot(gs[1, 3])
+    ax6 = ax2.twinx()
     ax3.set_xlim(0, 30)
     ax5.set_xlim(0, 30)
     ax3.set_ylim(0, 20)
@@ -133,13 +141,14 @@ def build_graphs(
     ax5.set_title('center distance')
     custom_lines = []
     custom_lines2 = []
-    for index, val in enumerate(zip(*multiple_frame_distances)):
+    ziped_frame_distances = list(zip(*multiple_frame_distances))
+    for index, val in enumerate(ziped_frame_distances):
         dist = list(val)
         color = next(color_cycle)
         ax3.plot(dist, color)
         custom_lines.append(Line2D([0], [0], label=f"{round(min(dist), 2)} - {round(max(dist), 2)} mm",
                                    color=color, lw=4))
-        custom_lines2.append(Line2D([0], [0], label='{:.2%}'.format(calculate_thickening(max(dist),min(dist))),
+        custom_lines2.append(Line2D([0], [0], label='{:.2%}'.format(calculate_thickening(max(dist), min(dist))),
                                     color=color, lw=4))
     fig.legend(title='Thickness', handles=custom_lines, loc='upper right')
     fig.legend(title='Thickening', handles=custom_lines2, loc='lower right')
@@ -157,10 +166,14 @@ def build_graphs(
     fig.legend(title='Thickening', handles=custom_lines2, loc='lower left')
     for frame in range(len(multiple_frame_snakes)):
         ax2.set_ylim(0, 20)
+        ax6.set_ylim(0, 1)
         ax4.set_ylim(10, 60)
         ax2.set_title('distance')
         ax4.set_title('center distance')
-        img = image.get_data()[:, :, frame]
+        if image_origin:
+            img = image_origin.get_data()[:, :, frame]
+        else:
+            img = image.get_data()[:, :, frame]
         ax.imshow(img)
         # Print Raw Snakes
         snakes = multiple_frame_snakes[frame]
@@ -175,6 +188,8 @@ def build_graphs(
             ax.plot(do[0], do[1], 'o', color=color)
 
             ax2.plot(count, distance, 'o', color=color)
+            val = abs(min(ziped_frame_distances[count][:frame+1])-distance)/min(ziped_frame_distances[count][:frame+1])
+            ax6.plot(count, val,'o', color='black')
             ax4.plot(count, center_distance, 'o', color=color)
 
             count += 1
@@ -184,6 +199,7 @@ def build_graphs(
         ax.clear()
         ax2.clear()
         ax4.clear()
+        ax6.clear()
 
 
 def snake_seg(np_pixdata, center_of_mass, seg_level: SegmentationLevel):
@@ -248,9 +264,10 @@ def save_fig_to_list(fig):
 
 
 # snake_segmentation_dots('/Users/antva-onioneto/Downloads/OUTPUT_TESTING/patient150_frame12.nii')
-#snake_segmentation_dots('/Users/antonioneto/Antonio/tese/Dados/OUTPUT_DIRECTORY_3D/Patient_CMD7_10.nii')
+# snake_segmentation_dots('/Users/antonioneto/Antonio/tese/Dados/OUTPUT_DIRECTORY_3D/Patient_CMD7_10.nii')
 # snake_segmentation_dots('/Users/antonioneto/Antonio/tese/Dados/OUTPUT_DIRECTORY_3D/Patient_MCH1_11.nii')
-snake_segmentation_dots('/Users/antonioneto/Antonio/tese/Dados/OUTPUT_DIRECTORY_3D/Patient_2_13.nii')
+snake_segmentation_dots('/Users/antonioneto/Antonio/tese/Dados/OUTPUT_DIRECTORY_3D/Patient_2_13.nii',
+                        '/Users/antonioneto/Antonio/tese/Dados/Nifti/Patient_2_13_0000.nii')
 imageio.mimsave('/Users/antonioneto/Downloads/animation.gif', fig_list)
 # show_gif_loop('/Users/antonioneto/Downloads/animation.gif')
 
